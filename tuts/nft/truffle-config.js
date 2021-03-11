dc283b69bb74c08541ebefaf770508aa4815fb77
Mehul Singh Rathore <mehulsingh@gmail.com>
2021-03-11 13:30:45 +0530

    created nft project

---
+++ tuts/nft/truffle-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  networks: {
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+
+  },
+
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+       version: "0.6.8",    // Fetch exact version from solc-bin (default: truffle's version)
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+       }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+}
