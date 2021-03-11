677829245380d59953551183aacccfdc25dfb274
Mehul Singh Rathore <mehulsingh@gmail.com>
2021-03-11 18:42:49 +0530

    did some work in contracts

--- tuts/nft/src/contracts/Color.sol
+++ tuts/nft/src/contracts/Color.sol
@@ -3,6 +3,17 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Color is ERC721 {
-    constructor() ERC721("MyCollectible", "MCO") public {
-    }
+  string[] public colors;
+  mapping(string => bool) _colorExists;
+
+  constructor() ERC721("Color", "COLOR") public {
+  }
+
+  function mint(string memory _color) public {
+    //Color - track it and add it
+    colors.push(_color);
+    uint _id = colors.length - 1;
+    _mint(msg.sender, _id);
+    _colorExists[_color] = true;
+  }
 }
